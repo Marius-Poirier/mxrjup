ad7cb6aec9d15cc8cd1bae003f9210ce0e9e0307
ASAP Marius <marius.poirierpro@gmail.com>
2026-02-17 12:48:57 +0100

    refactor: Externalize persistent data volumes to a dedicated directory and remove hardcoded WIP experiments.

--- personal-site/src/app/pages/wip/wip.ts
+++ personal-site/src/app/pages/wip/wip.ts
@@ -17,48 +17,5 @@ interface Experiment {
   styleUrl: './wip.scss'
 })
 export class WipComponent {
-  experiments = signal<Experiment[]>([
-    {
-      id: '1',
-      date: '2026.02.01',
-      object: 'Visual Identity',
-      subject: 'ASCII Renderer Engine',
-      link: '/'
-    },
-    {
-      id: '2',
-      date: '2026.01.28',
-      object: 'Poster',
-      subject: 'Luminous Strays',
-      link: '#'
-    },
-    {
-      id: '3',
-      date: '2026.01.15',
-      object: 'Simulation',
-      subject: 'Fluid Dynamics (WebGL)',
-      link: '#'
-    },
-    {
-      id: '4',
-      date: '2025.12.10',
-      object: 'Publication',
-      subject: 'The Brutalist Web',
-      link: '#'
-    },
-    {
-      id: '5',
-      date: '2025.11.05',
-      object: 'Exhibition',
-      subject: 'Glotta 01: On Monsters',
-      link: '#'
-    },
-    {
-      id: '6',
-      date: '2025.10.31',
-      object: 'Packaging',
-      subject: 'Cabinet of Commodities',
-      link: '#'
-    }
-  ]);
+  experiments = signal<Experiment[]>([]);
 }
